ebd0ab4a4f78f05079d99cfea80f49a4434fbcaf
Imam Nuralim <82601247+ImamNuralim@users.noreply.github.com>
2026-05-26 21:39:58 +0800

    feat: thermal print via TCP, printer selection, receipt formatting

--- resources/views/kasir/pos.blade.php
+++ resources/views/kasir/pos.blade.php
@@ -874,7 +874,7 @@
                     previewProduct: null,
                     longPressTimer: null,
                     printers: [
-    { label: 'Printer 1', id: 75492799 },
+    { label: 'Printer 1', id: 75492683 },
     { label: 'Printer 2', id: 75492736 },
     { label: 'Printer 3', id: 75492748 },
 ],
