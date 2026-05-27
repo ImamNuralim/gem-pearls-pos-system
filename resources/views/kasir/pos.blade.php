7272d3e4681218cff379942710dcf426b34265d6
Imam Nuralim <82601247+ImamNuralim@users.noreply.github.com>
2026-05-27 18:07:21 +0800

    final segara travel

--- resources/views/kasir/pos.blade.php
+++ resources/views/kasir/pos.blade.php
@@ -875,7 +875,7 @@
                     longPressTimer: null,
                     printers: [
     { label: 'Printer 1', id: 75492841 },
-    { label: 'Printer 2', id: 75496104 },
+    { label: 'Printer 2', id: 75496152 },
     { label: 'Printer 3', id: 75492748 },
 ],
                     selectedPrinter: null,
